22f5bb03a8e0114543ad3bb140cf40c331cc271f
Stijn Vandendriessche <vandendriessche.stijn@telenet.be>
2016-10-01 19:23:20 +0200

    Bug fixes

--- Labo1/blackjack.js
+++ Labo1/blackjack.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 "use strict"; // controleert of je var gebruikt om variabelen aan te maken; "veilig" JS schrijven
 
-var arrCards = new Array("ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"),
+var arrCards    = new Array("ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"),
 	deal,
 	hit,
 	stand,
@@ -31,11 +31,11 @@ function playCard(person, nummerOfCards) {
 		var random                = getRandom(arrCards.length);
 		div.style.backgroundImage = "url(images/" + arrCards[random] + eval(getRandom(3) + 1) + ".png)"
 
-		var transformDeg = Math.floor(Math.random() * 7) + 1;
+		var transformDeg    = Math.floor(Math.random() * 7) + 1;
 		transformDeg *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
 		div.style.transform = "rotate(" + transformDeg + "deg)";
 
-		var score = (random + 1) < 10? random + 1: 10;
+		var score = (random + 1) < 10 ? random + 1 : 10;
 
 		person.appendChild(div);
 
@@ -62,19 +62,35 @@ function winner(person) {
 	var message = document.getElementById("result");
 	if (person === dealer) {
 		message.innerHTML = "Dealer wins!";
+		disableControls(false, true, true);
+		setTimeout(function () {
+			document.getElementById("result").innerHTML = "";
+			resetGame();
+		}, 1500);
 	} else if (person === player) {
 		message.innerHTML = "Player wins!"
+		disableControls(false, true, true);
+		setTimeout(function () {
+			document.getElementById("result").innerHTML = "";
+			resetGame();
+		}, 1500);
 	}
 }
 
 function disableControls(dealButtonDisabled, hitButtonDisnabled, standButtonDisnabled) {
-	deal.disabled = dealButtonDisabled;
-	hit.disabled  = hitButtonDisnabled;
+	deal.disabled  = dealButtonDisabled;
+	hit.disabled   = hitButtonDisnabled;
 	stand.disabled = standButtonDisnabled;
 }
 
 function resetGame() {
-
+	var cards = document.querySelectorAll(".card");
+	for (var i = 0, len = cards.length; i < len; i++) {
+		cards[i].remove();
+	}
+	playerScore = 0;
+	dealerScore = 0;
+	checkScore();
 }
 
 function hitClick() {
@@ -93,9 +109,9 @@ function standClick() {
 }
 
 function initializeElements() {
-	deal  = document.getElementById("deal");
-	hit   = document.getElementById("hit");
-	stand = document.getElementById("stand");
+	deal   = document.getElementById("deal");
+	hit    = document.getElementById("hit");
+	stand  = document.getElementById("stand");
 	player = document.getElementById("player");
 	dealer = document.getElementById("dealer");
 }
